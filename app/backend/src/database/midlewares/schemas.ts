5a072a9c2f09e907538a41e7c3d167c76148639c
Veronica-Alfr <veagalves@gmail.com>
2022-08-19 21:54:21 -0300

    feat: realizado validação de email e password com Joi

---
+++ app/backend/src/database/midlewares/schemas.ts
@@ -0,0 +1,19 @@
+const Joi = require('joi');
+
+const fieldsMissing = 'All fields must be filled';
+
+const validateUser = (body: object) => {
+    const schemaUser = Joi.object({
+    email: Joi.string().email().required().messages({
+        'string.empty': fieldsMissing,
+    }),
+    password: Joi.string().required().min(7).messages({
+        'string.empty': fieldsMissing,
+    })
+});
+
+  const { error, value } = schemaUser.validate(body);
+    if (error) return { error: { code: 400, message: error.details[0].message } };
+
+    return value;
+};
