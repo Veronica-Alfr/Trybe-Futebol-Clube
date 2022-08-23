1c50290f6c3a862897eec6cd1d0859626819ffaa
Veronica-Alfr <veagalves@gmail.com>
2022-08-23 09:35:09 -0300

    fix: assinatura do jwt sign utilizando o process.env.JWT_SECRET

--- app/backend/src/database/services/jwtService.ts
+++ app/backend/src/database/services/jwtService.ts
@@ -1,7 +1,10 @@
 import jwt = require('jsonwebtoken');
+import 'dotenv/config';
+
+const secret = process.env.JWT_SECRET || ""
 
 export class JwtService {
   static sign(payload: { id: number, email: string }): string {
-    return jwt.sign(payload, 'SUPER SENHA');
+    return jwt.sign(payload, secret);
   }
 }
