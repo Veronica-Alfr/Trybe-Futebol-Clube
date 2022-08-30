6d7e7f42aa35e08e26bd9f7eb24c67b1c526e326
Veronica-Alfr <veagalves@gmail.com>
2022-08-30 18:01:24 -0300

    fix: importação errada de IMatchBody em IMatchService

--- app/backend/src/database/interfaces/IMatchService.ts
+++ app/backend/src/database/interfaces/IMatchService.ts
@@ -1,10 +1,10 @@
 import Match from "../models/matches";
-import { IMatchCreate } from "./IMatchCreate";
+import { IMatchBody } from "./IMatchBody";
 
 export interface IMatchService {
     list(): Promise<Match[]>,
     listInProgress(inProgress: boolean): Promise<Match[]>,
-    create({ homeTeam, awayTeam, homeTeamGoals, awayTeamGoals }: IMatchCreate): Promise<Match>,
+    create({ homeTeam, awayTeam, homeTeamGoals, awayTeamGoals }: IMatchBody): Promise<Match>,
     finish(id: number): Promise<void>,
-    changeMatchResult(id: number, { homeTeamGoals, awayTeamGoals }: IMatchCreate): Promise<void>,
+    changeMatchResult(id: number, { homeTeamGoals, awayTeamGoals }: IMatchBody): Promise<void>,
 }
