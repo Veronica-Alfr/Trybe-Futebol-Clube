2eed7dceae09653ee57e22cc0a2966e9fc662ab6
Veronica-Alfr <veagalves@gmail.com>
2022-09-17 21:43:33 -0300

    feat: add metodo sort para organização por totalPoints, etc

--- app/backend/src/database/services/leaderboardService.ts
+++ app/backend/src/database/services/leaderboardService.ts
@@ -87,6 +87,8 @@ export default class LeaderboardService {
             return home;
         });
 
-        return ranking;
+        return ranking.sort((a, b) => b.totalPoints - a.totalPoints || 
+        b.totalVictories - a.totalVictories || b.goalsBalance - a.goalsBalance ||
+        b.goalsFavor - a.goalsFavor || b.goalsOwn - a.goalsOwn);
     }
 }
